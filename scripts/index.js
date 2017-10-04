2b4a6d04771c86a9fa13bb1bb20ec1733c9b6a30
Jason Fan <jason.wc.fan@gmail.com>
2017-10-04 17:30:47 -0400

    added animation to move title from centre top left

--- scripts/index.js
+++ scripts/index.js
@@ -4,17 +4,27 @@ import { TweenMax } from 'gsap';
 import 'animation.gsap';
 import 'debug.addIndicators';
 
+/** Controller **/
 var controller = new ScrollMagic.Controller();
 
-var blockTween = new TweenMax.to('#block', 1.5, {
-    backgroundColor: 'red'
-});
-
-var containerScene = new ScrollMagic.Scene({
-    triggerElement: '.container'
+/** Tweens **/
+var titleTween = new TweenMax.to('#title', 1.5, {
+    top: 0,
+    left: 0
 })
-.setTween(blockTween)
+
+/** Scenes **/
+var moveToTopScene = new ScrollMagic.Scene({
+    triggerElement: '#who',
+    offset: '-50vh'
+})
+.setTween(titleTween)
 .addIndicators()
 .addTo(controller);
 
-console.log(blockTween);
+var containerScene = new ScrollMagic.Scene({
+    triggerElement: '#container'
+})
+.setPin('#title')
+.addIndicators()
+.addTo(controller);
